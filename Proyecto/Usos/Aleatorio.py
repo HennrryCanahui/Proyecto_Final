00baf9e37ec47f3c482996e672d98e6dd8988f94
HennrryCanahui <108434290+HennrryCanahui@users.noreply.github.com>
2023-10-04 16:04:16 -0600

    Update Aleatorio.py

--- Proyecto/Usos/Aleatorio.py
+++ Proyecto/Usos/Aleatorio.py
@@ -1,22 +1,16 @@
-from Conector import conexion
 import random
-cursor = conexion.cursor()
-def generar_numero_unico(cursor):
+from .Conector import conexion
+
+def generar_numero_unico():
     while True:
-        numero_generado = random.randint(1000, 9999)  # generador del identifiacor en un rango de 4 digitos
-        if not verificar_numero(numero_generado, cursor):
+        numero_generado = random.randint(1000, 9999)
+        if not verificar_numero(numero_generado, conexion):
             return numero_generado
 
 def verificar_numero(numero, cursor):
     query = "SELECT * FROM pruebas_1 WHERE Identificacion = %s"
-    cursor.execute(query, (numero,))
-    return cursor.fetchone() is not None
+    with cursor.cursor() as cursor:
+        cursor.execute(query, (numero,))
+        return cursor.fetchone() is not None
 
 
-# variable de almacenamieto del numero generado 
-
-"""numero_a_verificar = generar_numero_unico(cursor)"""
-
-# testeo que si funciono el numeor genrado
-
-"""print(numero_a_verificar)"""
