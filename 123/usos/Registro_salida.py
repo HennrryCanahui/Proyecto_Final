0b2d5eda3351767921ae3adb26ed175f73ec3663
HennrryCanahui <108434290+HennrryCanahui@users.noreply.github.com>
2023-10-24 15:29:33 -0600

    Update Registro_salida.py

--- 123/usos/Registro_salida.py
+++ 123/usos/Registro_salida.py
@@ -2,10 +2,10 @@ import flet as ft
 import mysql.connector
 from datetime import datetime
 
+# Función para establecer una conexión a la base de datos MySQL.
 def establecer_conexion():
-    
-    #Función para establecer una conexión a la base de datos MySQL.
     try:
+        # Establecer la conexión a la base de datos MySQL
         conexion = mysql.connector.connect(
             user="root",
             password="",
@@ -21,66 +21,87 @@ def establecer_conexion():
     except mysql.connector.Error as e:
         print(f"Error al conectar a la base de datos: {e}")
 
-def obtener_fecha_hora_actual():
-        now = datetime.now()
-        fecha_actual = now.strftime("%d/%m/%Y")
-        hora_actual = now.strftime("%I:%M:%S %p")
-        return fecha_actual, hora_actual
+# Esta función obtiene la fecha actual en el formato "dd/mm/yyyy"
+def obtener_fecha_actual():
+    now = datetime.now()
+    fecha_actual = now.strftime("%d/%m/%Y")
+    return fecha_actual
 
+# Esta función obtiene la hora actual en el formato "hh:mm AM/PM"
+def obtener_hora_actual():
+    now = datetime.now()
+    hora_actual = now.strftime("%I:%M %p")
+    return hora_actual
+
+# Contiene el apartado gráfico
 def Entradas(page: ft.Page):
 
     # Establecer conexión a la base de datos
     conexion = establecer_conexion()
 
+    # Crea una barra que contiene un título
     page.appbar = ft.AppBar(
-        title=ft.Text("Registro Entrada"),
+        title=ft.Text("Registro Salida"),
         center_title=True,
         bgcolor=ft.colors.SURFACE_VARIANT,
     )
 
-    def Existencia_id(conexion):
+    # Función para encontrar el No. del registro que cumple ciertas condiciones
+    def encontrar_no(conexion):
         id_empleado = Empleado.value
-        query = "SELECT * FROM registros WHERE ID = %s AND fecha_entrada = CURDATE() AND (hora_salida IS NULL OR hora_salida = '')"
-    
-        with conexion.cursor() as cursor:
-            cursor.execute(query, (id_empleado,))
-            result = cursor.fetchone()
+        fecha_entrada = obtener_fecha_actual()
+        query_verificacion =  "SELECT * FROM registros WHERE ID = %s AND fecha_entrada = %s AND Hora_Salida = ''"
+        
+        cursor = conexion.cursor()
+        cursor.execute(query_verificacion, (id_empleado, fecha_entrada))
+        result = cursor.fetchone()
+        cursor.close()
 
-        if result is not None:
-        # La identificación existe y hay un registro con la hora de salida vacía, continuar con normalidad
-            return True
+        if result:
+            return result[0]  # Devuelve el valor de la columna "No."
         else:
-        # La identificación no existe o ya hay un registro con la hora de salida, mostrar alerta
-            alerta = ft.AlertDialog(
-                title=ft.Text("Registro Existente"),
-                content=ft.Text("Ya existe un registro para este ID y fecha de entrada con hora de salida."),
-            )
-            page.dialog = alerta
-            alerta.open = True
-            page.update()
-            return False
-        
+            return None  # No se encontró un registro válido
+
+    # Función para actualizar la hora de salida en el registro encontrado
+    def actualizar_hora_salida(conexion, no_registro):
+        cursor = conexion.cursor()
+
+        # Actualizar la columna "Hora_Salida" para el registro encontrado
+        query_actualizacion = "UPDATE registros SET Hora_Salida = %s WHERE `No.` = %s"
+        hora_salida_nueva = obtener_hora_actual()
+        cursor.execute(query_actualizacion, (hora_salida_nueva, no_registro))
+
+        # Confirmar la transacción
+        conexion.commit()
+
+        # Cerrar el cursor
+        cursor.close()
+
+    # Función que se ejecuta al hacer clic en el botón "Registrar"
     def button_clicked(e):
-        
-        if not Existencia_id(conexion):
-            # La identificación no existe, no continuar con el proceso
-            return
-    
-        """fecha_actual, hora_entrada = obtener_fecha_hora_actual()
+        # Encontrar el No. del registro que cumple las condiciones
+        no_registro_encontrado = encontrar_no(conexion)
 
-        # Resto del código para la inserción en la base de datos
-        sql = "INSERT INTO registros (fecha_entrada, hora_salida) VALUES (%s, %s)"
-        val = (Empleado.value, fecha_actual, hora_entrada,)
+        if no_registro_encontrado is not None:
+            actualizar_hora_salida(conexion, no_registro_encontrado)
+            dlg = ft.AlertDialog(
+            title=ft.Text("    ¡Registro Exitoso!"),
+            )
+            page.dialog = dlg
+            dlg.open = True
+            Empleado.value = ""
+            page.update()
+        else:
+            dlg = ft.AlertDialog(
+            title=ft.Text("No se encontró un registro válido"),
+            )
+            page.dialog = dlg
+            dlg.open = True
+            Empleado.value = ""
+            page.update()
 
-        with conexion.cursor() as cursor:
-            cursor.execute(sql, val)
-            conexion.commit()
-            print(cursor.rowcount, "record inserted.")"""
-
-    
-    
-     # Crear elementos de la interfaz de usuario
-    Empleado = ft.TextField(label="Nombres")
+    # Crear elementos de la interfaz de usuario
+    Empleado = ft.TextField(label="ID")
     Validacion = ft.ElevatedButton(text="Registrar", on_click=button_clicked)
     
     row_1 = ft.Row(controls=[
@@ -95,5 +116,8 @@ def Entradas(page: ft.Page):
         ft.Container(expand=1),
     ])
     
+    # Agregar elementos a la página
+    page.add(row_1, row_2)
 
+# Ejecutar la aplicación
 ft.app(target=Entradas)
